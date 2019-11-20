54069a5b2e0955b8afc19e89a2376bb40500dfb1
Camilo <c.acevedo1993@gmail.com>
2019-11-19 22:02:30 -0500

    Added action decorator

--- libros/views.py
+++ libros/views.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from libros.models import Libro
 from libros.serializers import LibroSerializer, CreateLibroSerializer
+
+from autores.serializers import AutorSerializer
+from editoriales.serializers import EditorialSerializer
 
 # Create your views here.
 class LibroViewSet(viewsets.ModelViewSet):
@@ -14,3 +19,17 @@ class LibroViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return CreateLibroSerializer
         return LibroSerializer
+    
+    @action(detail=True, method=['GET'])
+    def autores(self, request, pk=None):
+        libro = self.get_object()
+        autores = libro.autores.all()
+        serializer = AutorSerializer(autores, many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+    @action(detail=True, method=['GET'])
+    def editoriall(self, request, pk=None):
+        libro = self.get_object()
+        editorial = libro.editorial
+        serilizer = EditorialSerializer(editorial)
+        return Response(status=status.HTTP_200_OK, data=serilizer.data)
